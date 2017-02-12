f1a7b7b60884187b5aeac584e1e0f72e5ed6b8df
Cameron <cameron.bennett@northumbria.ac.uk>
2017-02-12 00:21:54 +0000

    Inital Commit
    ^^

---
+++ src/FullScreenCaptureExample.java
@@ -0,0 +1,33 @@
+import java.awt.AWTException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+ 
+import javax.imageio.ImageIO;
+ 
+/**
+ * This program demonstrates how to capture a screenshot
+ * as an image which will be saved into a file. (Screenshot.jpeg)
+ *
+ */
+public class FullScreenCaptureExample {
+ 
+    public static void main(String[] args) {
+        try {
+            Robot robot = new Robot();
+            String format = "jpg"; //Low bandwidth
+            String fileName = "Screenshot." + format;
+             
+            Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+            BufferedImage screenFullImage = robot.createScreenCapture(screenRect);
+            ImageIO.write(screenFullImage, format, new File(fileName));
+             
+            //System.out.println("A full screenshot saved!");
+        } catch (AWTException | IOException ex) {
+            System.err.println(ex);
+        }
+    }
+}
